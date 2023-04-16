05aa1ec610849a3f23269e8142d22a9b2278fa08
raul_zannoni <raul.zannnoni92@gmail.com>
2023-04-16 20:15:09 +0200

    2nd commit

--- AlgoPHP/Partie2/exo6.php
+++ AlgoPHP/Partie2/exo6.php
@@ -1,35 +1,34 @@
 <h1>Exercise 6</h1>
 
-
 <p>Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau de valeurs.<br>
 <h4>Exemple :</h4>
 <code>$elements = array("Monsieur","Madame","Mademoiselle");</code><br>
 <code>alimenterListeDeroulante($elements);</code>
 
-
 <h2>Résultat</h2>
 
+<style>
+
+/* style de la boite */
+div {
+    display: block;
+    margin: 8px 0;
+    border-radius: 5px;
+    background-color: lightblue;
+    padding: 12px 20px;
+    width: fit-content;
+}
+
+</style>
+
 <?php
-/*
-<label for="pet-select">Choose a pet:</label>
-
-<select name="pets" id="pet-select">
-    <option value="">--Please choose an option--</option>
-    <option value="dog">Dog</option>
-    <option value="cat">Cat</option>
-    <option value="hamster">Hamster</option>
-    <option value="parrot">Parrot</option>
-    <option value="spider">Spider</option>
-    <option value="goldfish">Goldfish</option>
-</select>
-
-*/
 
 //fonction qui crée le formulaire
 function alimenterListeDeroulante($array)
     {   
         //titre de la liste deroulante
-        $result =  "<label for='civilité'>Civilité:</label><br>";
+        $result =  "<div><label for='civilité'>Civilité:</label><br>";
+
         //debut de le formulaire avec selection default
         $result .= "<select name='titre_civilité'> id='civilité-select'>
                             <option value=''>-- --</option>";
@@ -39,14 +38,15 @@ function alimenterListeDeroulante($array)
             {
                 $result .=  "<option value=".$valeur.">".$valeur."</option><br>";
             }
+
         //fin de la liste deroulante
-        $result .=  "</select>";
+        $result .=  "</select></div>";
 
         //imprime de le formulaire
         echo $result;
     }
 
-//array de les mots à afficher dans le formulaire deroulante
+//tableau de les mots à afficher dans le formulaire deroulante
 $elements = [   "Monsieur",
                 "Madame",
                 "Mademoiselle"
