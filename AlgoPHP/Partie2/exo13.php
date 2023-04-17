cdbc28fe4f5fa2b07c0b6319f7776d067a5fd5d6
raul_zannoni <raul.zannnoni92@gmail.com>
2023-04-17 10:53:48 +0200

    part1exo15: class personne + méthode calcAge()

--- AlgoPHP/Partie2/exo13.php
+++ AlgoPHP/Partie2/exo13.php
@@ -2,15 +2,38 @@
 
 <p>Créer une classe Voiture possédant les propriétés suivantes : <code> marque</code>, <code>modèle</code>, <code>nbPortes</code> et <code>vitesseActuelle</code> ainsi que les méthodes <code>demarrer( )</code>, <code>accelerer( )</code> et <code>stopper( )</code> en plus des accesseurs (<code>get</code>) et mutateurs (<code>set</code>) traditionnels. La vitesse initiale de chaque véhicule instancié est de 0. Une méthode personnalisée pourra afficher toutes les informations d’un véhicule.<br>
 
-<p>v1 ➔ "Peugeot","408",5 <br>
-v2 ➔ "Citroën","C4",3<br>
+<p>v1 ➔ <code>"Peugeot"</code>,<code>"408"</code>,<code>5</code> <br>
+v2 ➔ <code>"Citroën"</code>,<code>"C4"</code>,<code>3</code><br>
 <p>Coder l’ensemble des méthodes, accesseurs et mutateurs de la classe tout en réalisant des jeux de tests pour vérifier la cohérence de la classe Voiture.<br>
 
-<p>Bonus : ajouter une méthode <code>ralentir(vitesse)</code> dans la classe Voiture.
+<p><b>Bonus</b> : ajouter une méthode <code>ralentir(vitesse)</code> dans la classe Voiture.
 
 <h2>Résultat</h2>
 
 <?php
 
+class Voiture
+    {
+        private string $_marque;
+        private string $_modele;
+        private int $_nBPortes;
+        private float $_vitesseActuelle = 0;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 ?>
